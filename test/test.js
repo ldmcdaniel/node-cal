01def0c7249be7bd0a54a6d529f1dafaccc0d697
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-23 13:56:15 -0500

    working test coverage collection

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
+var path = require('path');
 var should = require('chai').should();
+var Cal = require(path.join(process.cwd(), '/app'))
 
 describe('Mocha + Chai', function() {
   it('truthyness', function () {
