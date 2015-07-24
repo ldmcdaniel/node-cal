b65e47a29e5d8d1202171a40a34cb8ad10d1100f
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-24 14:22:10 -0500

    working zeller function testing with various dates

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-var zellers = require('./lib/zeller');
+var zeller = require('./lib/zeller');
 
 console.log(zellers);
 
-// process.argv.forEach(function(val, index, array) {
-//   if(index >= 2 ) {
-//     console.log(index + ': ' + val);
-//   }
-// });
+
--- test/zellar.test.js
+++ test/zellar.test.js
@@ -1,16 +1,28 @@
 var path = require('path');
 var should = require('chai').should();
-var zellers = require(path.join(process.cwd(), './lib/zeller'));
+var zeller = require(path.join(process.cwd(), './lib/zeller'));
 
 
-describe('zellers', function () {
-  it('should be a number', function () {
-    zellers.should.be.a('number');
+describe('zeller', function () {
+  it('should be equal to cal date', function () {
+    zeller(15, 3, 1918).should.equal(6);
   });
-  it('should be between between 0 & 6', function () {
-    zellers.should.be.within(0, 6);
+  it('should be equal to cal date', function () {
+    zeller(19, 1, 1924).should.equal(0);
   });
-  it('should be a whole number between 0 & 6', function () {
-    [0,1,2,3,4,5,6].should.include.members([zellers])
-  })
+  it('should be equal to cal date', function () {
+    zeller(28, 2, 1931).should.equal(0);
+  });
+  it('should be equal to cal date', function () {
+    zeller(29, 2, 1904).should.equal(2);
+  });
+  it('should be equal to cal date', function () {
+    zeller(1, 3, 1900).should.equal(5);
+  });
+  // it('should be between between 0 & 6', function () {
+  //   zeller.should.be.within(0, 6);
+  // });
+  // it('should be a whole number between 0 & 6', function () {
+  //   [0,1,2,3,4,5,6].should.include.members([zeller])
+  // })
 })
