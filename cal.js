5d59b7618b6be53a4c5664e1a9c6dc02b65288a4
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-07-26 16:55:30 -0500

    fully working node cal month year || year

--- cal.js
+++ cal.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
-var zeller = require('./lib/zeller');
-var month = require('./lib/month');
-var year = require('./lib/year');
+// var zeller = require('./lib/zeller');
+if(process.argv.length === 3 && Number(process.argv[2]) > 12) {
+  var year = require('./lib/year');
+} else if (process.argv.length === 4 && Number(process.argv[2]) < 13 && Number(process.argv[3]) > 12) {
+  var month = require('./lib/month');
+}
+
+
