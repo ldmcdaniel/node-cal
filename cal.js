43287916d777727d2e48dd37de66b1e6557477ba
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-09-14 21:13:05 -0500

    updated commits

--- cal.js
+++ cal.js
@@ -3,6 +3,7 @@
 var month = require('./lib/month');
 var year = require('./lib/year');
 // var zeller = require('./lib/zeller');
+var URLParse = require('./lib/calURLParse');
 if (process.argv.length === 2) {
   month();
 } else if(process.argv.length === 3 && Number(process.argv[2]) > 1752 && process.argv.length === 3 && Number(process.argv[2]) < 10000) {
@@ -10,7 +11,5 @@ if (process.argv.length === 2) {
 } else if (process.argv.length === 4 && Number(process.argv[2]) < 13 && Number(process.argv[3]) > 1752){
   month()
 } else {
-  console.log('I am sorry, but remember to:\n  Type "node cal" to display the current month\n  Type "node cal yyyy" to display a calendar for a specific year\n  Type "node cal mm yyyy" to display a specific month for a specific year')
-}
-
-
+  console.log('\nERROR!  Please enter:\n\n  "node cal" to display the current month.\n  "node cal yyyy" to display the calendar for a specific year.\n  "node cal mm yyyy" to display a specific month and a specific year.\n') }
+// URLParse(process.argv[2]);
